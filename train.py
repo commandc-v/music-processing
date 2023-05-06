4b789fd353a258d66fc1c418787e01cfac282e21
Gugo-le <rngustmd0719@naver.com>
2023-05-06 15:30:25 +0900

    create data loader

--- train.py
+++ train.py
@@ -9,6 +9,27 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+BATCH_SIZE = 128
+
+class FeedForwardNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.dense_layers = nn.Sequential(
+            nn.Linear(28*28, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10)
+        )
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_data):
+        flattened_data = self.flatten(input_data)
+        logits = self.dense_layers(flattened_data)
+        predictions = self.softmax(logits)
+        return predictions 
+
+
 def download_mnist_datasets():
     train_data = datasets.MNIST(
         root = "data",
