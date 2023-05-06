ea88f9f58a1c537c9f89ecf4c750cdab595aabc5
Gugo-le <rngustmd0719@naver.com>
2023-05-06 20:51:41 +0900

    Running the inference script

--- inference.py
+++ inference.py
@@ -14,6 +14,15 @@ class_mapping = [
     "9"
 ]
 
+def predict(model, input, target, class_mapping):
+    model.eval()
+    with torch.no_grad():
+        predictions = model(input)
+        # Tensor (1, 10) -> [ [0.1, 0.01, ..., 0.6]]
+        predicted_index = predictions[0].argmax(0)
+        predicted = class_mapping[predicted_index]
+        expected = class_mapping[target]
+    return predicted, expected    
 
 if __name__ == "__main__":
     # load back the model
@@ -29,3 +38,5 @@ if __name__ == "__main__":
 
     # make an inference
     predicted, expected = predict(feed_forward_net, input, target, class_mapping)
+
+    print(f"Predicted: '{predicted}', expected: '{expected}'")
