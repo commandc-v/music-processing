b09b480fc8db0b6e37ff935132e441980cb57cbd
Gugo-le <rngustmd0719@naver.com>
2023-05-06 20:22:49 +0900

    class mapping

--- inference.py
+++ inference.py
@@ -1,12 +1,31 @@
 import torch
 from train import FeedForwardNet, download_mnist_datasets
 
+class_mapping = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+]
+
 
 if __name__ == "__main__":
-# load back the model
-feed_forward_net = FeedForwardNet()
-state_dict = torch.load("feedforwardnet.pth")
-feed_forward_net.load_state_dict(state_dict)
-# load MNIST validation dataset
-# get a sample from the validation dataset
-# make an inference
+    # load back the model
+    feed_forward_net = FeedForwardNet()
+    state_dict = torch.load("feedforwardnet.pth")
+    feed_forward_net.load_state_dict(state_dict)
+
+    # load MNIST validation dataset
+    _, validation_data = download_mnist_datasets()
+
+    # get a sample from the validation dataset
+    input, target = validation_data[0][0], validation_data[0][1]
+
+    # make an inference
+    predicted, expected = predict(feed_forward_net, input, target, class_mapping)
